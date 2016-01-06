9c3b240edbc450d11bb1a90308ef1f02db1c7821
Kelsey Bones <kelseybones@hotmail.co.uk>
2016-01-06 12:46:05 +0000

    Added Section 5&7 and started the map js

---
+++ app.js
@@ -0,0 +1,10 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: -28.643387, lng: 153.612224},
+    zoomControl: true,
+    zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+    }
+  });
+}
